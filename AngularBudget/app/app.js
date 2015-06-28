e6c2e51d3a421148a57945094b6cb4b621a02e40
RiaMang <1riamanglani@gmail.com>
2015-06-28 00:05:39 -0400

    6-27-2015
    take 2

---
+++ AngularBudget/app/app.js
@@ -0,0 +1,42 @@
+﻿
+
+(function () {
+
+
+    var app = angular.module('app', ['ui.router']);
+
+    app.config(function ($stateProvider, $urlRouterProvider) {
+
+        //$urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+
+            .state('group', {
+
+                templateUrl: 'group.html'
+            })
+            .state('dashboard', {
+                templateUrl: 'dashboard.html'
+            })
+            .state('accounts', {
+                templateUrl: 'accounts.html'
+            })
+            .state('accounts.create', {
+                //url: '/list',
+                templateUrl: 'accounts-create.html'
+            })
+            .state('accounts.edit', {
+                templateUrl: 'accounts-edit.html'
+            })
+            .state('transactions', {
+                templateUrl: 'transactions.html'
+            })
+            .state('budgetItems', {
+                templateUrl: 'budgetItems.html'
+            })
+            .state('budget', {
+                templateUrl: 'budget.html'
+            });
+
+    });
+})();
